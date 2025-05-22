22b9660c7a918a0e8439a07210d09583648bebe1
Ed <ed@Laptop-von-Ed.local>
2025-05-23 00:05:57 +0200

    доавление строк

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = 'Мой проект';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 100;
+const rollback = 10;
+const fullPrice = 500;
+const adaptive = true;
+
+alert('Первый раз работаю с гитом');
+
+console.log('у меня мак, поэтому все работает через костыли');
